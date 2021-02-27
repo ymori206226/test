1ca347b3d86683f036e4c00fca8e26a3c8a917ac
tsuchimoc <user.name>
2021-02-27 20:41:56 +0900

    test

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import datetime
 
 from src import config as cf
 from src import mpilib as mpi
-from src.fileio import error, print_geom, prints
+from src.fileio import error, prints
 from src.read import read_input
+from src.init import QuketData
 
 prints(
     "///////////////////////////////////////////////////////////////////////////////////",
@@ -42,13 +43,13 @@ prints(
     "///                                                                             ///"
 )
 prints(
-    "///                      Quantum Computing Simulator Ver 0.3.1                  ///"
+    "///                      Quantum Computing Simulator Ver 0.4 dev                ///"
 )
 prints(
     "///                                                                             ///"
 )
 prints(
-    "///        Copyright 2019-2020                                                  ///"
+    "///        Copyright 2019-2021                                                  ///"
 )
 prints(
     "///        QC Project Team, Ten-no Research Group                               ///"
@@ -72,46 +73,43 @@ Finish = False
 job_no = 0
 cf.geom_update = False
 while Finish is False:
+    Quket = QuketData()
     job_no += 1
-    Finish = read_input(job_no)
-    if cf.n_active_electrons == 0:
+    Finish = read_input(Quket, job_no)
+    # Finish = read_input(job_no)
+    if Quket.n_electron == 0:
         error("# electrons = 0 !")
-    if cf.basis != "hubbard":
-        if cf.n_active_orbitals == 0:
+    if Quket.basis != "hubbard":
+        if Quket.n_orbital == 0:
             error("# orbitals = 0 !")
     else:
-        if cf.hubbard_nx == 0:
+        if Quket.hubbard_nx == 0:
             error("Hubbard model but hubbard_nx is not defined!")
-        cf.n_orbitals = cf.hubbard_nx * cf.hubbard_ny
+        Quket.n_orbital = Quket.hubbard_nx * Quket.hubbard_ny
 
     if cf.opt_method == "L-BFGS-B":
         opt_options = {
             "disp": True,
-            "maxiter": cf.maxiter,
-            "gtol": cf.gtol,
-            "ftol": cf.ftol,
+            "maxiter": Quket.maxiter,
+            "gtol": Quket.gtol,
+            "ftol": Quket.ftol,
             "eps": cf.eps,
             "maxfun": cf.maxfun,
         }
     elif cf.opt_method == "BFGS":
         opt_options = {
             "disp": True,
-            "maxiter": cf.maxiter,
-            "gtol": cf.gtol,
+            "maxiter": Quket.maxiter,
+            "gtol": Quket.gtol,
             "eps": cf.eps,
         }
 
     if cf.pyscf_guess == "read":
         cf.pyscf_guess = "chkfile"
 
-    from openfermion.transforms import jordan_wigner
-
-    from src.init import set_initial_det
-    from src.opelib import generate_operators, get_hubbard
-    from src.phflib import set_projection
+    from src.init import QuketData
     from src.vqe import VQE_driver
-    from src.utils import fci2qubit
-    from src.fileio import print_state
+    from src.qite.qite import QITE_driver
 
     prints("+-------------+")
     prints("|  Job # %3d  |" % job_no)
@@ -122,147 +120,71 @@ while Finish is False:
         )
     )
 
-    if cf.basis == "hubbard":
-        cf.model = 'hubbard'
-        if mpi.main_rank:
-            jw_hamiltonian, jw_s2 = get_hubbard(
-                cf.hubbard_u,
-                cf.hubbard_nx,
-                cf.hubbard_ny,
-                cf.n_active_electrons,
-                cf.run_fci,
-            )
-            prints(
-                "Hubbard model: nx = %d  " % cf.hubbard_nx,
-                "ny = %d  " % cf.hubbard_ny,
-                "U = %2.2f" % cf.hubbard_u,
-            )
-        else:
-            jw_hamiltonian = None
-            jw_s2 = None
-        cf.n_active_orbitals = cf.hubbard_nx * cf.hubbard_ny
-        jw_hamiltonian = mpi.comm.bcast(jw_hamiltonian, root=0)
-        jw_s2 = mpi.comm.bcast(jw_s2, root=0)
+    if Quket.basis == "hubbard":
+        model = "hubbard"
+    elif "heisenberg" in Quket.basis:
+        model = "heisenberg"
+    else:
+        model = "chemical"
 
-    else: 
-        cf.model = 'chemical'
-        if cf.geometry is None:
-            error("No geometry specified.")
-        elif cf.geom_update:
-            # Set Jordan-Wigner Hamiltonian and S2 operators using PySCF and Open-Fermion
-            generate_operators(
-                cf.pyscf_guess,
-                cf.geometry,
-                cf.basis,
-                cf.multiplicity,
-                cf.charge,
-                cf.n_active_electrons,
-                cf.n_active_orbitals,
-            )
-            jw_hamiltonian = jordan_wigner(cf.Hamiltonian_operator)
-            jw_s2 = jordan_wigner(cf.S2_operator)
-            cf.geom_update = False
-            print_geom(cf.geometry)
-            prints("E[FCI] = ", cf.fci_energy)
-            prints("E[HF]  = ", cf.hf_energy)
-            if cf.print_level > 2:
-                prints("jw_hamiltonian:\n", jw_hamiltonian)
+    #############################
+    #                           #
+    #    Construct QuketData    #
+    #                           #
+    #############################
+    Quket.initialize(pyscf_guess=cf.pyscf_guess)
+    # Transform Jordan-Wigner Operators to Qulacs Format
+    Quket.jw_to_qulacs()
+    # Set projection parameters
+    Quket.set_projection()
 
-
-    # If maxiter = 0, skip the VQE part. This option is useful to do PySCF for different geometries
-    # (to read and utilize initial guess HF orbitals, which sometimes can change by occupying the wrong orbitals).
-    if cf.maxiter == 0:
+    if Quket.ansatz is None or Quket.maxiter == 0:
         continue
 
-    # Check spin, multiplicity, and Ms
-    if cf.spin == -1:
-        cf.spin = cf.Ms + 1  # Default
-    if (cf.spin - cf.Ms - 1) % 2 != 0 or cf.spin < cf.Ms + 1:
-        prints("Spin = {}    Ms = {}".format(cf.spin, cf.Ms))
-        error("Spin and Ms not cosistent.")
-    if (cf.n_active_electrons + cf.multiplicity - 1) % 2 != 0:
-        prints(
-            "Incorrect specification for n_electrons = {} and multiplicity = {}.".format(
-                cf.n_active_electrons, cf.multiplicity
-            )
-        )
-    cf.nalpha = (cf.n_active_electrons + cf.multiplicity - 1)//2
-    cf.nbeta  = cf.n_active_electrons - cf.nalpha
-    #cf.fci_state = fci2qubit(cf.n_active_orbitals,cf.nalpha,cf.nbeta,cf.fci_coeff)
-    #if cf.print_fci:
-    #    print_state(cf.fci_state, name="(FCI)", threshold=cf.print_amp_thres)
-
-
-    # Check initial determinant
-    if cf.det == -1:
-        # Initial determinant is RHF or ROHF
-        set_initial_det()
-    cf.current_det = cf.det
-
-    if cf.method in ("phf", "suhf", "sghf", "opt_puccsd", "opt_puccd"):
-        cf.SpinProj = True
-    if cf.SpinProj or cf.NumberProj:
-        if cf.method not in (
-            "uccsd",
-            "uccd",
-            "jmucc",
-            "uhf",
-            "phf",
-            "suhf",
-            "sghf",
-            "opt_puccsd",
-            "opt_puccd",
-        ):
-            prints("Spin-Projection is not yet available for {}.".format(cf.method))
-        #elif cf.method in ("uccd", "uccsd"):
-        #    cf.method = "p" + cf.method
-
-        set_projection()
-    # VQE part
-    VQE_driver(
-        jw_hamiltonian,
-        jw_s2,
-        cf.method,
-        cf.kappa_guess,
-        cf.theta_guess,
-        cf.mix_level,
-        cf.rho,
-        cf.DS,
-        cf.opt_method,
-        opt_options,
-        cf.print_level,
-        cf.maxiter,
-        cf.Kappa_to_T1,
-        cf.print_amp_thres,
-    )
-
-    # post VQE for excited states
-    nexcited = len(cf.excited_states)
-    for istate in range(nexcited):
-        cf.lower_states.append(cf.States)
-        prints(
-            "Performing VQE for excited states: {}/{} states".format(
-                istate + 1, nexcited
-            )
-        )
-        cf.current_det = cf.excited_states[istate]
+    #############
+    # VQE part  #
+    #############
+    if Quket.method == "vqe":
         VQE_driver(
-            jw_hamiltonian,
-            jw_s2,
-            cf.method,
-            "zero",
+            Quket,
+            cf.kappa_guess,
             cf.theta_guess,
             cf.mix_level,
-            cf.rho,
-            cf.DS,
             cf.opt_method,
             opt_options,
             cf.print_level,
-            cf.maxiter,
-            False,
-            cf.print_amp_thres,
+            Quket.maxiter,
+            cf.Kappa_to_T1,
         )
-    prints("End at ", datetime.datetime.now())  # time stamp
+
+        # post VQE for excited states
+        for istate in range(Quket.nexcited):
+            Quket.lower_states.append(Quket.state)
+            prints(
+                "Performing VQE for excited states: {}/{} states".format(
+                    istate + 1, Quket.nexcited
+                )
+            )
+            Quket.det = Quket.excited_states[istate]
+            VQE_driver(
+                Quket,
+                "zero",
+                cf.theta_guess,
+                cf.mix_level,
+                cf.rho,
+                cf.DS,
+                cf.opt_method,
+                opt_options,
+                cf.print_level,
+                Quket.maxiter,
+                False,
+            )
+    ##############
+    # QITE part  #
+    ##############
+    elif Quket.method == "qite":
+        QITE_driver(Quket)
+    prints("Normal termination of quket at ", datetime.datetime.now())  # time stamp
     if mpi.main_rank and os.path.exists(cf.tmp):
         os.remove(cf.tmp)
     # VQE part done, go to the next job.
